584f85f25b39809dfca18d5da6a41e7444831819
aedmorales <admorales@up.edu.ph>
2023-11-13 11:59:52 +0800

    export

--- drills.py
+++ drills.py
@@ -5,3 +5,5 @@ df.insert(0, "ID", [i for i in range(len(df.index))])
 df["Date"] = "META-" + df["Date"]
 df.insert(2, "Dummy", [r.randint(0,1) for _ in range(len(df.index))])
 print(df)
+
+df.to_csv("table2.csv")  
