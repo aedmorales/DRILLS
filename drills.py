cea199a06a885a63c403df65cb61a223c5f6ab01
aedmorales <admorales@up.edu.ph>
2023-11-13 12:03:35 +0800

    add ID

--- drills.py
+++ drills.py
@@ -1,6 +1,7 @@
 import pandas as pd  
 import random as r
 
+df = pd.read_csv ("table1.csv").dropna()
 df.insert(0, "ID", [i for i in range(len(df.index))])
 df["Date"] = "META-" + df["Date"]
 df.insert(2, "Dummy", [r.randint(0,1) for _ in range(len(df.index))])
