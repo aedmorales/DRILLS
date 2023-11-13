c91959d3e77ce1be7fe1084e0263010436697055
aedmorales <admorales@up.edu.ph>
2023-11-13 11:58:36 +0800

    add dummy column

--- drills.py
+++ drills.py
@@ -3,3 +3,5 @@ import random as r
 
 df.insert(0, "ID", [i for i in range(len(df.index))])
 df["Date"] = "META-" + df["Date"]
+df.insert(2, "Dummy", [r.randint(0,1) for _ in range(len(df.index))])
+print(df)
